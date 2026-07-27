154e0ba35db5b973f224063f6519c84fd7535add
esehmec <esehmec@gmail.com>
2026-07-26 23:58:10 -0500

    Add more Monitor properties to after new scheduling changes

--- src/main/java/com/eyyupsehmec/ortakkuran/config/MonitorProperties.java
+++ src/main/java/com/eyyupsehmec/ortakkuran/config/MonitorProperties.java
@@ -1,24 +1,88 @@
 package com.eyyupsehmec.ortakkuran.config;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
 import java.time.Duration;
 
-@Getter
-@Setter
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
 @ConfigurationProperties(prefix = "monitor")
 public class MonitorProperties {
 
-    @NotBlank
+    //TODO: USE LOMBOK
     private String url;
+    private int threshold;
+    private Duration interval;
+    private FastCheck fastCheck = new FastCheck();
 
-    private Duration interval = Duration.ofHours(1);
+    public void setSkipBucket(int skipBucket) {
+        this.skipBucket = skipBucket;
+    }
 
-    @Min(1)
-    private int threshold = 5;
+    private int skipBucket;
 
+
+    public int getSkipBucket() {
+        return skipBucket;
+    }
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getThreshold() {
+        return threshold;
+    }
+
+    public void setThreshold(int threshold) {
+        this.threshold = threshold;
+    }
+
+    public Duration getInterval() {
+        return interval;
+    }
+
+    public void setInterval(Duration interval) {
+        this.interval = interval;
+    }
+
+    public FastCheck getFastCheck() {
+        return fastCheck;
+    }
+
+    public void setFastCheck(FastCheck fastCheck) {
+        this.fastCheck = fastCheck;
+    }
+
+    public static class FastCheck {
+
+        private int minPages;
+        private int maxPages;
+        private Duration interval;
+
+        public int getMinPages() {
+            return minPages;
+        }
+
+        public void setMinPages(int minPages) {
+            this.minPages = minPages;
+        }
+
+        public int getMaxPages() {
+            return maxPages;
+        }
+
+        public void setMaxPages(int maxPages) {
+            this.maxPages = maxPages;
+        }
+
+        public Duration getInterval() {
+            return interval;
+        }
+
+        public void setInterval(Duration interval) {
+            this.interval = interval;
+        }
+    }
 }
